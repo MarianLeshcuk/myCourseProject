1b5fd095516e7822987567db0a970fa7a908a7d0
Marian <leshchukmarian@gmail.com>
2018-04-27 13:29:08 +0300

    some basic changes

--- src/app/app.routing-module.ts
+++ src/app/app.routing-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './autorization/sign-in/sign-in.component';
 import { SignUpComponent } from './autorization/sign-up/sign-up.component';
+// import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  {path: 'signIn', component: SignInComponent},
-  {path: 'signUp', component: SignUpComponent}
+  // {path: '', component: MainComponent},
+  {path: 'signIn', component: SignInComponent, outlet: 'autorization'},
+  {path: 'signUp', component: SignUpComponent, outlet: 'autorization'}
 ];
 
 @NgModule({
